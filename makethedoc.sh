cbce745c97617e8b954e75ed470707705e3cccbe
Jan Heiland <jnhlbd@gmail.com>
2019-11-22 10:41:14 +0100

    cleanup

--- makethedoc.sh
+++ makethedoc.sh
@@ -4,11 +4,11 @@ pandoc concept.md -o concept-slides.html \
     -V revealjs-url=https://revealjs.com \
     -V theme=solarized -V hash=true \
     -V viewDistance=15 -V width=1280 -V height=960 -V margin=0.05
-# pandoc concept.md -o index.html --filter pandoc-citeproc -t revealjs --slide-level=2 -s -H <(echo '\['; cat def.tex; echo '\]')  --mathjax='https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML' -V revealjs-url=https://revealjs.com -V theme=solarized -V viewDistance=15 -V width=1280 -V height=720 -V margin=0.1
+
+# ## base theme slides -- works well
 pandoc concept.md --filter pandoc-citeproc -t beamer -o base-theme-slides.pdf -H def.tex
 pandoc concept.md --filter pandoc-citeproc -t beamer -o base-theme-slides.tex -H def.tex
 
-pandoc concept.md --slide-level 2 -t beamer -o slides-body.tex
 
 # THEMEOPS=CSC,layouttwo,english,adjustframetitle
 # pandoc concept.md --filter pandoc-citeproc -t beamer --slide-level=2 -o concept-slides.pdf -H def.tex -V classoption:aspectratio=43 -V theme=mpi2015 -V themeoptions=$THEMEOPS
@@ -16,18 +16,16 @@ pandoc concept.md --slide-level 2 -t beamer -o slides-body.tex
 #     --filter pandoc-citeproc --natbib -t beamer --slide-level=2 \
 #     -H def.tex --table-of-contents \
 #     -V classoption:aspectratio=43 -V theme=mpi2015 -V themeoptions=$THEMEOPS
-# 
-# sed -i 's/ignorenonframetext,//' concept-slides.tex
-# sed -i 's/\\frame{\\titlepage}/\\maketitle/' concept-slides.tex
-# sed -i 's/\\begin{frame}/\\begin{frame}\\frametitle/g' concept-slides.tex
+
+# ## just create the body of the slides
+pandoc concept.md --slide-level 2 -t beamer -o slides-body.tex
+# ## delete the bottom 3 lines (where there is the references)
 sed -i '$d' slides-body.tex
 sed -i '$d' slides-body.tex
 sed -i '$d' slides-body.tex
+
 pdflatex -interaction=nonstopmode mpi-style-skeleton.tex
 pdflatex -interaction=nonstopmode mpi-style-skeleton.tex
 bibtex mpi-style-skeleton.aux
 pdflatex -interaction=nonstopmode mpi-style-skeleton.tex
 latexmk -c
-
-# pdflatex -interaction=nonstopmode mpi-style-skeleton.tex
-# pdflatex -interaction=nonstopmode mpi-style-skeleton.tex

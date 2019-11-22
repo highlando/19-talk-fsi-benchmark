d62556cdbb1872714d61219475463f99cd010300
Jan Heiland <jnhlbd@gmail.com>
2019-11-22 10:42:41 +0100

    no need for these files here

--- prop-to-ghp.sh
+++
@@ -1,9 +0,0 @@
-git checkout prep-for-ghp
-git merge master
-source makethedoc.sh
-git add index.html
-git commit -m 'prop from master'
-git checkout gh-pages
-git merge prep-for-ghp
-git push origin gh-pages
-git checkout master

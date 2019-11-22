6f4c422c0dd9a9786b251f5832d08e9a2feb44f1
Jan Heiland <jnhlbd@gmail.com>
2019-11-22 10:39:06 +0100

    now mpi style using a sceleton

--- makethedoc.sh
+++ makethedoc.sh
@@ -8,8 +8,26 @@ pandoc concept.md -o concept-slides.html \
 pandoc concept.md --filter pandoc-citeproc -t beamer -o base-theme-slides.pdf -H def.tex
 pandoc concept.md --filter pandoc-citeproc -t beamer -o base-theme-slides.tex -H def.tex
 
+pandoc concept.md --slide-level 2 -t beamer -o slides-body.tex
+
 # THEMEOPS=CSC,layouttwo,english,adjustframetitle
 # pandoc concept.md --filter pandoc-citeproc -t beamer --slide-level=2 -o concept-slides.pdf -H def.tex -V classoption:aspectratio=43 -V theme=mpi2015 -V themeoptions=$THEMEOPS
-# pandoc concept.md -o concept-slides.tex --filter pandoc-citeproc --natbib -t beamer --slide-level=2 -H def.tex --table-of-contents -V classoption:aspectratio=43 -V theme=mpi2015 -V themeoptions=$THEMEOPS
+# pandoc concept.md -o concept-slides.tex \
+#     --filter pandoc-citeproc --natbib -t beamer --slide-level=2 \
+#     -H def.tex --table-of-contents \
+#     -V classoption:aspectratio=43 -V theme=mpi2015 -V themeoptions=$THEMEOPS
+# 
 # sed -i 's/ignorenonframetext,//' concept-slides.tex
 # sed -i 's/\\frame{\\titlepage}/\\maketitle/' concept-slides.tex
+# sed -i 's/\\begin{frame}/\\begin{frame}\\frametitle/g' concept-slides.tex
+sed -i '$d' slides-body.tex
+sed -i '$d' slides-body.tex
+sed -i '$d' slides-body.tex
+pdflatex -interaction=nonstopmode mpi-style-skeleton.tex
+pdflatex -interaction=nonstopmode mpi-style-skeleton.tex
+bibtex mpi-style-skeleton.aux
+pdflatex -interaction=nonstopmode mpi-style-skeleton.tex
+latexmk -c
+
+# pdflatex -interaction=nonstopmode mpi-style-skeleton.tex
+# pdflatex -interaction=nonstopmode mpi-style-skeleton.tex
